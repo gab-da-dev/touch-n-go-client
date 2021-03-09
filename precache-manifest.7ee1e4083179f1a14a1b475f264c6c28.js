10b84e192706749dc78a00133656e4b7452035e4
Gabriel <gabriel@genex.co.za>
2021-03-09 23:22:38 +0200

    Updates

---
+++ precache-manifest.7ee1e4083179f1a14a1b475f264c6c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7a6ba5ee320625b1b5270d2eab15ae",
+    "url": "/gab-da-dev/touchngo.co.za/index.html"
+  },
+  {
+    "revision": "b61db63a1f826b7ff508",
+    "url": "/gab-da-dev/touchngo.co.za/static/css/main.a38ad579.chunk.css"
+  },
+  {
+    "revision": "55cdad4e5df857fbd2cb",
+    "url": "/gab-da-dev/touchngo.co.za/static/js/2.17ef1a84.chunk.js"
+  },
+  {
+    "revision": "485dbfbd3160f858746c6901ccc6c1fa",
+    "url": "/gab-da-dev/touchngo.co.za/static/js/2.17ef1a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61db63a1f826b7ff508",
+    "url": "/gab-da-dev/touchngo.co.za/static/js/main.87c25eb0.chunk.js"
+  },
+  {
+    "revision": "38dd55953a8855d61504",
+    "url": "/gab-da-dev/touchngo.co.za/static/js/runtime-main.b37faff8.js"
+  }
+]);
