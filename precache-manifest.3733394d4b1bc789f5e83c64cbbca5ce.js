f71384a967e2445a3e593d9ac6b161c9c8f3e35e
Gabriel <gabriel@genex.co.za>
2021-03-10 00:06:09 +0200

    Updates

---
+++ precache-manifest.3733394d4b1bc789f5e83c64cbbca5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01c764c340beae1bf4a043216f963ec",
+    "url": "/github.com/gab-da-dev/touchngo.co.za/index.html"
+  },
+  {
+    "revision": "ecc10ba47aefe2cb26ef",
+    "url": "/github.com/gab-da-dev/touchngo.co.za/static/css/main.a38ad579.chunk.css"
+  },
+  {
+    "revision": "55cdad4e5df857fbd2cb",
+    "url": "/github.com/gab-da-dev/touchngo.co.za/static/js/2.17ef1a84.chunk.js"
+  },
+  {
+    "revision": "485dbfbd3160f858746c6901ccc6c1fa",
+    "url": "/github.com/gab-da-dev/touchngo.co.za/static/js/2.17ef1a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc10ba47aefe2cb26ef",
+    "url": "/github.com/gab-da-dev/touchngo.co.za/static/js/main.e6b2c3dc.chunk.js"
+  },
+  {
+    "revision": "832b7db396f08b27183f",
+    "url": "/github.com/gab-da-dev/touchngo.co.za/static/js/runtime-main.3ac42027.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7607559129be178a15cca0414dc8d0af.js"
+  "/github.com/gab-da-dev/touchngo.co.za/precache-manifest.3733394d4b1bc789f5e83c64cbbca5ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/gab-da-dev/touchngo.co.za/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
